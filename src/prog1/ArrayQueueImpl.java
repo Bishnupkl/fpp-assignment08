a551e352ed45eefd328891a0ca6534e5f668efbf
Kai <40851151+Kheang1409@users.noreply.github.com>
2024-06-14 07:33:20 -0500

    prog1

--- src/prog1/ArrayQueueImpl.java
+++ src/prog1/ArrayQueueImpl.java
@@ -32,13 +32,13 @@ public class ArrayQueueImpl {
         }
     }
     public int dequeue(){
-        if(isEmpty()) return 0;
+        if(isEmpty()) return -1;
         int delete = peek();
         front = front + 1;
         return delete;
     }
     public int peek(){
-        if(isEmpty()) return 0;
+        if(isEmpty()) return -1;
         return arr[front];
     }
 
@@ -66,45 +66,5 @@ public class ArrayQueueImpl {
         }
         sb.append("]");
         return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        //Resize
-        //Reuse Free Memory
-        ArrayQueueImpl myQueue = new ArrayQueueImpl();
-        myQueue.enqueue(1);
-        myQueue.enqueue(2);
-        System.out.println("Dequeue: " + myQueue.dequeue());
-        myQueue.enqueue(3);
-        myQueue.enqueue(4);
-        myQueue.enqueue(5);
-        myQueue.enqueue(6);
-        myQueue.enqueue(7);
-        myQueue.enqueue(8);
-        myQueue.enqueue(9);
-        myQueue.enqueue(10);
-        System.out.println(myQueue + " size:" + myQueue.size());
-        System.out.println("Peek: " + myQueue.peek());
-        System.out.println("Dequeue: " + myQueue.dequeue());
-        System.out.println("Dequeue: " + myQueue.dequeue());
-        System.out.println(myQueue + " size:" + myQueue.size());
-        myQueue.enqueue(11);
-        myQueue.enqueue(12);
-        System.out.println(myQueue + " size:" + myQueue.size());
-        myQueue.enqueue(13);
-        myQueue.enqueue(14);
-        myQueue.enqueue(15);
-        myQueue.enqueue(16);
-        System.out.println("Peek: " + myQueue.dequeue());
-        System.out.println("Peek: " + myQueue.dequeue());
-        myQueue.enqueue(17);
-        myQueue.enqueue(18);
-        myQueue.enqueue(19);
-        myQueue.enqueue(20);
-        System.out.println("Peek: " + myQueue.dequeue());
-        System.out.println("Peek: " + myQueue.dequeue());
-        System.out.println("Peek: " + myQueue.dequeue());
-
-        System.out.println(myQueue + " size:" + myQueue.size());
     }
 }
