e31efdf3a29ee50409c8f5cb52e3b81c8ca44a8a
Kai <40851151+Kheang1409@users.noreply.github.com>
2024-06-13 21:08:27 -0500

    test Class for prog4

--- src/prog4/TicketingSystem.java
+++ src/prog4/TicketingSystem.java
@@ -70,19 +70,4 @@ public class TicketingSystem {
         }
         return sb.toString();
     }
-    public static void main (String[] args){
-        TicketingSystem ticketingSystem = new TicketingSystem();
-        ticketingSystem.addTicket("Anxiety");
-        ticketingSystem.addTicket("Depressed");
-        ticketingSystem.addTicket("PTSD");
-        ticketingSystem.addTicket("DID");
-        System.out.println(ticketingSystem);
-        System.out.println(STR."View Next Ticket: \{ticketingSystem.viewNextTicket()}");
-        System.out.println(STR."Process Ticket: \{ticketingSystem.processTicket()}");
-        System.out.println(STR."After Of Process Ticket: \{ticketingSystem}");
-        System.out.println(STR."Process Ticket: \{ticketingSystem.processTicket()}");
-        System.out.println(STR."Process Ticket: \{ticketingSystem.processTicket()}");
-        System.out.println(STR."Process Ticket: \{ticketingSystem.processTicket()}");
-        System.out.println(ticketingSystem);
-    }
 }
